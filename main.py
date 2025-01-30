13e4a8c022acf7065a7c99d9066b6d8e6ab4473b
Your Name <you@example.com>
2025-01-30 06:52:03 -0500

    2nd

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+
+    dt = 0
+    clock = pygame.time.Clock()
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+
         screen.fill(color='black')
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 if __name__ == '__main__':
+    player = Player(x = SCREEN_WIDTH/2, y = SCREEN_HEIGHT/2)
     main()
 
 
